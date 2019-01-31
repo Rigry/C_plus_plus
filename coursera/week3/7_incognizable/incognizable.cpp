d677d803883c0b60ace9fab833157bd41add3be7
Alexandr <arigry@gmail.com>
2019-01-31 21:18:04 +0300

    week3 incognizable

--- coursera/week3/7_incognizable/incognizable.cpp
+++ coursera/week3/7_incognizable/incognizable.cpp
@@ -1,11 +1,21 @@
+struct Incognizable
+{
+   int x {0};
+   int y {0};
+   Incognizable(){}
+   Incognizable(int i) : x{i} {}
+   Incognizable(int _1, int _2) : x{_1}, y{_2} {}
+};
+
+
 // Определите тип Incognizable, для которого следующий код будет корректен:
-// int main() {
-//   Incognizable a;
-//   Incognizable b = {};
-//   Incognizable c = {0};
-//   Incognizable d = {0, 1};
-//   return 0;
-// }
+int main() {
+  Incognizable a;
+  Incognizable b = {};
+  Incognizable c = {0};
+  Incognizable d = {0, 1};
+  return 0;
+}
 
 // struct Incognizable {
 //   int x = 0;
