b18c0a0f5996475eacd50fa72bc82589a75984b8
Alexandr <arigry@gmail.com>
2019-01-31 21:12:21 +0300

    week3 reversible_string

--- coursera/week3/6_reversible_strings/reversible_string.cpp
+++ coursera/week3/6_reversible_strings/reversible_string.cpp
@@ -1,33 +1,13 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+
+using namespace std;
+
 // Реализуйте класс ReversibleString, хранящий строку и поддерживающий 
 // методы Reverse для переворота строки и ToString для получения строки.
 // Пример
 // Код
-
-// int main() {
-//   ReversibleString s("live");
-//   s.Reverse();
-//   cout << s.ToString() << endl;
-  
-//   s.Reverse();
-//   const ReversibleString& s_ref = s;
-//   string tmp = s_ref.ToString();
-//   cout << tmp << endl;
-  
-//   ReversibleString empty;
-//   cout << '"' << empty.ToString() << '"' << endl;
-  
-//   return 0;
-// }
-
-// Вывод
-
-// evil
-// live
-// ""
-
-
-// #include <algorithm>
-// #include <string>
 
 // class ReversibleString {
 // public:
@@ -44,4 +24,42 @@
 // private:
 //   string data;
 // };
+
+class ReversibleString
+{
+   string s;
+public:   
+   ReversibleString(){s.clear();}
+   ReversibleString(const string& str) : s{str} {}
+   void Reverse(){reverse(s.begin(), s.end());}
+   string ToString() const {return s;}
+};
+
+int main() {
+  ReversibleString s("live");
+  s.Reverse();
+  cout << s.ToString() << endl;
+  
+  s.Reverse();
+  const ReversibleString& s_ref = s;
+  string tmp = s_ref.ToString();
+  cout << tmp << endl;
+  
+  ReversibleString empty;
+  cout << '"' << empty.ToString() << '"' << endl;
+  
+  return 0;
+}
+
+// Вывод
+
+// evil
+// live
+// ""
+
+
+// #include <algorithm>
+// #include <string>
+
+
 
